77df1b344c72ed045ea0711ccda32b6687d14be1
joaoapass <112397879+joaoapass@users.noreply.github.com>
2022-11-03 18:22:58 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,19 +1,71 @@
-function validacaoEmail(email){
-  var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-  var check=/@[w-]+./;
-  var checkend=/.[a-zA-Z]{2,3}$/;
-  if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){
-    return ;
-  }
-  else {return true;}
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', (event) =>{
+  event.preventDefault()
+
+  check();
+})
+
+function check(){
+  const nome = document.getElementById("nome");
+  const email = document.getElementById("email");
+  const mensagem = document.getElementById("mensagem");
+  const error = document.getElementById("erros");
+
+  if(email.value == ''){
+    email.classList.add("erro");
+    email.classList.remove("ok");
     
-  
+  }else{
+    email.classList.remove("erro");
+    email.classList.add("ok");
+    error.innerHTML = "";
   }
 
-  function IsEmail(){
-    var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-    var check=/@[w-]+./;
-    var checkend=/.[a-zA-Z]{2,3}$/;
-    if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
-    else {return true;}
+
+  if(nome.value == ''){
+    nome.classList.add("erro");
+    nome.classList.remove("ok");
+    
+  }else{
+    nome.classList.remove("erro");
+    nome.classList.add("ok");
+    error.innerHTML = "";
+  }
+
+
+  if(mensagem.value == ''){
+    mensagem.classList.add("erro");
+    mensagem.classList.remove("ok");
+    
+  }else{
+    mensagem.classList.remove("erro");
+    mensagem.classList.add("ok");
+  }
+
+  if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    error.innerHTML = "Email invalido";
+    email.classList.add("erro");
+    email.classList.remove("ok");
+  }else{
+    error.innerHTML = "";
+    email.classList.remove("erro");
+    email.classList.add("ok");
+  }
+
+  const emailOk = document.getElementsByClassName("email");
+  const emailValid = [ ... emailOk].every((emailok)=>{
+    return (emailok.className === "email ok")
+  })
+  const nomeOk = document.getElementsByClassName("nome");
+  const nomeValid = [ ... nomeOk].every((nomeok)=>{
+    return (nomeok.className === "nome ok")
+  })
+  const mensagemOk = document.getElementsByClassName("mensagem");
+  const mensagemValid = [ ... mensagemOk].every((mensagemok)=>{
+    return (mensagemok.className === "mensagem ok")
+  })
+
+  if(emailValid && nomeValid && mensagemValid){
+  }
 }
